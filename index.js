6737d7c2d2f61b3f2c1509fb525c3056968133bd
Lucas9785 <lucasmatheus993@gmail.com>
2025-07-17 20:43:35 -0300

    adicionado o controler e a rota

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
 import express from 'express'
-import { use } from 'react'
+import userRoutes from './src/routes/user.routes.js';
 const app = express()
+
 app.use(express.json()) // Middleware para analisar JSON no corpo da requisição
-
-const users = []
-
-//1° Parte da Rota é
-// Metodos possiveis de rota GET, POST, PUT, DELETE
-//2° Parte da Rota é o Name =>/ - sempre no plural
-// cal
-
-app.post('/users', (req, res) => {
-  const body = req.body
-  users.push(body)
-  res.status(201).send('User created successfully')
-})
-app.get('/users', (req, res) => {
-  res.json({message:"Estes são os usuários", users})
-})
+app.use(userRoutes)
 
 
 app.listen(3000, () => {
---
+++ src/controller/user.controllers.js
@@ -0,0 +1,16 @@
+import userServices  from "../service/user.services.js";
+
+async function createUserController(req, res) {
+  const newUser = req.body;
+  try {
+    const user = await userServices.createUserService(newUser);
+    res.status(201).send({user})
+  } catch (error) {
+    return res.status(400).send(error.message);
+  }
+}
+
+
+export default {
+  createUserController
+}
